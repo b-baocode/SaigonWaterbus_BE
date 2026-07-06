9d87beb752d898ccfae94698fad33b71af813882
b-baocode <161267408+b-baocode@users.noreply.github.com>
2026-07-07 02:19:17 +0700

    test: add empty ProbeAutoMigrate migration to detect auto-migration

---
+++ src/Infrastructure/Data/Migrations/20260706181925_ProbeAutoMigrate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaigonWaterbus.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProbeAutoMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
